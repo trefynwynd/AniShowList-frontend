337edcc38f3a7d8beb39ded3e930d03aaaa785b3
edwardglez <gonzalez.edward@outlook.com>
2020-11-17 16:24:46 -0800

    minor frontend work

--- src/config/Routes.js
+++ src/config/Routes.js
@@ -5,6 +5,8 @@ import Home from '../pages/Home'
 import Register from '../pages/Register'
 import Login from '../pages/Login'
 import Profile from '../pages/Profile'
+import Favorite from '../pages/Favorites'
+import WatchingList from '../pages/WatchingList'
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
   const currentUser = localStorage.getItem('id')
@@ -17,6 +19,8 @@ const PrivateRoute = ({ component: Component, ...rest }) => {
 const Routes = (props) => (
   <Switch>
     <Route exact path='/' component={ Home } />
+    <Route path='/favorites' component={ Favorite } />
+    <Route path='/watching-list' component={ WatchingList } />
     <Route path='/register' component={ Register } />
     <Route path='/login' render={ (routeComponentProps) => {
       return  <Login 
