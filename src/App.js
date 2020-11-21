5c786d1589c320f25830186192bb6fed10a701a0
martinacruz <cruz.s.martin@gmail.com>
2020-11-20 17:01:53 -0800

    connected api to App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Routes from './config/Routes'
@@ -7,6 +8,22 @@ import UserModel from './models/user'
 
 function App() {
   const [currentUser, setCurrentUser] = useState(localStorage.getItem('id'))
+
+  const [ search, setSearch ] = useState([])
+  const [ isLoading, setIsLoading ] = useState(true)
+  const [ anime, setAnime ] = useState()
+
+  const fetchSearch = () => {
+    return axios.get(`https://api.jikan.moe/v3/search/anime?q=naruto`)
+    .then(result => {
+      setAnime(result.data.results)
+      console.log(result.data.results)
+    })
+  }
+
+  useEffect(() => {
+    fetchSearch()
+  }, [])
 
   const storeUser = (userId) => {
     localStorage.setItem('id', userId)
