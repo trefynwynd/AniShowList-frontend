ea432b404507a558684fe4cc602b6185e56d38cd
trefynwynd <jl.alejandria@gmail.com>
2020-11-21 17:03:36 -0800

    Changed the welcome text.

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 const Profile = props => {
   return (
-    <h1>Profile of user with ID { props.currentUser }</h1>
+    <>
+      <div>
+        <h1>Profile & Watching list of <br /> user with ID { props.currentUser }</h1>
+      </div>
+    </>
   )
 }
 
