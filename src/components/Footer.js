47d84a25b8ca0084d76129cb1503edc347c8fea8
trefynwynd <jl.alejandria@gmail.com>
2020-11-21 17:09:12 -0800

    Updated the position of the footer and replaced the placeholder text with different placeholder text.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
+
+import './footer.css'
 
 const Footer = () => {
-  return <p>Footer Here!</p>
+  return (
+    <>
+      <footer>
+        <ul>
+          <li><p>AniShowList</p></li>
+          <li><p>Copyright 2020 - SEIR921</p></li>
+          <li><Link to={'/about'}>About This Project</Link></li>
+        </ul>
+      </footer>
+    </>
+  )
 }
 
 export default Footer;
