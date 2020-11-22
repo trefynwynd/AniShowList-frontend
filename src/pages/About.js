9571c76267b8bb4aee46a82f2b568055997d4a1c
trefynwynd <jl.alejandria@gmail.com>
2020-11-21 17:05:12 -0800

    Created an About page to have information about our group.

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div>
+    <h1>About This Project</h1>
+      <p></p>
+      <p></p>
+      <p></p>
+    </div>
+  )
+}
+
+export default About
