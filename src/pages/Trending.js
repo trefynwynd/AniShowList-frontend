0939e4357ca70ebeddddcf7706e38f615d4eec70
trefynwynd <jl.alejandria@gmail.com>
2020-11-21 17:04:12 -0800

    Added a new page for the Trending list.

---
+++ src/pages/Trending.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Trending() {
+    return (
+        <div>
+            <h1>What anime series are Trending at the moment!</h1>
+            <h2>Trending..Stay tuned.</h2>
+        </div>
+    )
+}
+
+export default Trending
