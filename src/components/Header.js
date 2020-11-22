3c23011dbbc2b6ef67cbf8be15c937be6de296d6
trefynwynd <jl.alejandria@gmail.com>
2020-11-21 17:07:42 -0800

    Added and rearranged the links to better serve the layout for users and potential users.

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,14 +14,13 @@ const Header = (props) => {
           { props.currentUser ? 
             <>
               <li><Link to={'/profile'}>Profile</Link></li>
-              <li><a href="/logout" onClick={ props.logout }>Log Out</a></li>
               <li><Link to={'/favorites'}>Favorites</Link></li>
               <li><Link to={'/watching-list'}>Watching List</Link></li>
+              <li><a href="/logout" onClick={ props.logout }>Log Out</a></li>
             </>
           :
             <>
-              <li><Link to={'/favorites'}>Favorites</Link></li>
-              <li><Link to={'/watching-list'}>Watching List</Link></li>
+              <li><Link to={'/trending'}>trending</Link></li>
               <li><Link to={'/register'}>Register</Link></li>
               <li><Link to={'/login'}>Login</Link></li>
             </>
