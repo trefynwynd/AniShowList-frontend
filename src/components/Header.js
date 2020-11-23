b8839d42f558b6ae0f1b4f4bcd4023b41aa8744d
trefynwynd <jl.alejandria@gmail.com>
2020-11-23 11:25:29 -0800

    Added the Trending link.

--- src/components/Header.js
+++ src/components/Header.js
@@ -15,12 +15,13 @@ const Header = (props) => {
             <>
               <li><Link to={'/profile'}>Profile</Link></li>
               <li><Link to={'/favorites'}>Favorites</Link></li>
+              <li><Link to={'/trending'}>Trending</Link></li>
               <li><Link to={'/watching-list'}>Watching List</Link></li>
               <li><a href="/logout" onClick={ props.logout }>Log Out</a></li>
             </>
           :
             <>
-              <li><Link to={'/trending'}>trending</Link></li>
+              <li><Link to={'/trending'}>Trending</Link></li>
               <li><Link to={'/register'}>Register</Link></li>
               <li><Link to={'/login'}>Login</Link></li>
             </>
