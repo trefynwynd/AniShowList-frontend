4692b0b57133d1f1844ad752644d81d99b6c66c8
trefynwynd <jl.alejandria@gmail.com>
2020-11-30 00:54:35 -0800

    Testing out that POST route for the favorites still.

--- src/pages/ShowDetail.js
+++ src/pages/ShowDetail.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import { useParams } from 'react-router-dom'
+import usershow from '../models/usershow'
 
-const ShowDetail = () => {
+const ShowDetail = props => {
 
     const { id } = useParams()
     //console.log(id)
@@ -20,11 +21,27 @@ const ShowDetail = () => {
         selectedShow()
     }, [id])
 
+
+    handleSubmit = (event) => {
+      event.preventDefault()
+  
+      usershow.create(this.state)
+      .then(data => {
+        // redirecting with react router
+        this.props.history.push('/favorites')
+      })
+  
+    }
+
     return (
         <div className="show-title">
             <h1>{show.title}</h1>
             <button>Add to Watchlist</button>
+
+            <form onSubmit={ this.handleSubmit }>
+            <input type="hidden" name="apiId" value="{show.mal_id}" />
                 <button>Add to Favorites</button>
+                </form>
             <div className="show-image">
                 <img src={show.image_url} alt="" /><br /><br />
                 <div className="show-synopsis">
